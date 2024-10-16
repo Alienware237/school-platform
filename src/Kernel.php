0dbdba62bfb80d460b5db18cf1db49fee024a665
Kevin Piam <kevinpiam3@yahoo.com>
2024-10-16 21:04:33 +0200

    Feat: Initial file

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace okpt\school\platform;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
